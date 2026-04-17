03c3d9a34464370f67390a232f705e02338179fc
Joel <joeldario9912@gmail.com>
2026-04-17 17:32:08 -0300

    Rename app container to header

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,6 @@
 import { headerUI } from "./ui/header";
 
 document.addEventListener("DOMContentLoaded", () => {
-  const app = document.getElementById("app") as HTMLDivElement;
+  const app = document.getElementById("header") as HTMLDivElement;
   app.appendChild(headerUI);
 });
