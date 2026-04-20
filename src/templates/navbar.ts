f55fbdcd30f4db178576ca87eb6050410407e3e5
Joel <joeldario9912@gmail.com>
2026-04-20 15:34:44 -0300

    feat: cart counter visor added

--- src/templates/navbar.ts
+++ src/templates/navbar.ts
@@ -1,10 +1,13 @@
+import { counterVisor } from "./counter-visor";
+
 export const navBarTemplate = () => {
   const navBar = document.createElement("nav") as HTMLElement;
+  const counterElem = counterVisor();
 
   navBar.innerHTML = `
     <ul>
       <li><a href="/src/pages/catalog/catalog.html">Catalog</a></li>
-      <li><a href="/src/pages/cart/cart.html">Cart<span id="cart-counter"></span></a></li>
+      <li><a href="/src/pages/cart/cart.html">Cart${counterElem}</a></li>
     </ul>
   `;
 
